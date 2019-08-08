771286a209266f855c4ce7dd9be23400c252d958
Gary Taylor <gary.taylor@hismessages.com>
2019-08-08 14:26:16 +0100

    Upgraded client to latest

--- config/initializers/ccd_client.rb
+++ config/initializers/ccd_client.rb
@@ -3,7 +3,12 @@ EtCcdClient.config do |c|
   c.idam_base_url = ENV.fetch('CCD_IDAM_BASE_URL', 'http://localhost:5000')
   c.data_store_base_url = ENV.fetch('CCD_DATA_STORE_BASE_URL', 'http://localhost:4452')
   c.document_store_base_url = ENV.fetch('CCD_DOCUMENT_STORE_BASE_URL', 'http://localhost:4506')
-  c.document_store_url_rewrite = ENV.fetch('CCD_DOCUMENT_STORE_URL_REWRITE', 'localhost:4506:dm-store:8080')
+  if ENV.fetch('CCD_DOCUMENT_STORE_URL_REWRITE', 'false') == 'false'
+    c.document_store_url_rewrite = false
+  else
+    c.document_store_url_rewrite = ENV.fetch('CCD_DOCUMENT_STORE_URL_REWRITE', 'localhost:4506:dm-store:8080').split(':')
+  end
+  puts "config.document_store_url_rewrite=#{c.document_store_url_rewrite}"
   c.gateway_api_url = ENV.fetch('CCD_GATEWAY_API_URL', 'http://localhost:3453')
   c.jurisdiction_id = ENV.fetch('CCD_JURISDICTION_ID', 'EMPLOYMENT')
   c.microservice = ENV.fetch('CCD_MICROSERVICE_ID', 'ccd_gw')
