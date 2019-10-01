1e8237f257dc0bc7fde90a7f5596cd63c0a1965d
Gary Taylor <gary.taylor@hismessages.com>
2019-10-01 17:25:09 +0100

    Now validating in_progress and complete events for single claims

--- app/services/export_claim_service.rb
+++ app/services/export_claim_service.rb
@@ -16,17 +16,18 @@ class ExportClaimService
   attr_accessor :client_class, :disallow_file_extensions
 
   def do_export(export, bid:, jid:)
+    send_claim_export_started_event(bid, export, jid)
     client_class.use do |client|
       case_type_id = export.dig('external_system', 'configurations').detect {|c| c['key'] == 'case_type_id'}['value']
       resp = client.caseworker_start_case_creation(case_type_id: case_type_id)
       event_token = resp['token']
       data = ClaimPresenter.present(export['resource'], event_token: event_token, files: files_data(client, export), ethos_case_reference: ethos_case_reference(export.dig('resource', 'office_code')))
       created_case = client.caseworker_case_create(data, case_type_id: case_type_id)
-      send_claim_exported_event(bid, export, jid, created_case['id'], case_type_id)
+      send_claim_exported_event(bid, export, jid, created_case['id'], created_case.dig('case_data', 'ethosCaseReference'), case_type_id)
     end
   end
 
-  def send_claim_exported_event(bid, export, jid, case_id, case_type_id)
+  def send_claim_exported_event(bid, export, jid, case_id, case_reference, case_type_id)
     event_data = {
       sidekiq: {
         jid: jid,
@@ -35,10 +36,25 @@ class ExportClaimService
       export_id: export['id'],
       external_data: {
         case_id: case_id,
+        case_reference: case_reference,
         case_type_id: case_type_id
       }
     }
     send_application_event('ClaimExportSucceeded', event_data)
+  end
+
+  def send_claim_export_started_event(bid, export, jid)
+    event_data = {
+      sidekiq: {
+        jid: jid,
+        bid: bid,
+      },
+      export_id: export['id'],
+      external_data: {},
+      state: 'in_progress',
+      percent_complete: 0
+    }
+    send_application_event('ClaimExportFeedbackReceived', event_data)
   end
 
 end
---
+++ spec/support/external_events.rb
@@ -0,0 +1,36 @@
+require 'json'
+module EtCcdExport
+  module Test
+    module ExternalEventsMethods
+      def external_events
+        ExternalEvents.new
+      end
+    end
+    class ExternalEvents
+      include RSpec::Matchers
+      def assert_claim_export_succeeded(export:, ccd_case:)
+        jobs = Sidekiq::Worker.jobs.select { |j| j['queue'] == 'events' && j['wrapped'] == 'TriggerEventJob' && j['args'].first['arguments'].first == 'ClaimExportSucceeded' }
+        expect(jobs.length).to be 1
+        parsed_job_data = JSON.parse(jobs.first['args'].first['arguments'][1])
+        expect(parsed_job_data).to include 'export_id' => export.id
+        expect(parsed_job_data['external_data']).to include 'case_id' => ccd_case['id'],
+                                                            'case_reference' => ccd_case['case_fields']['ethosCaseReference'],
+                                                            'case_type_id' => 'Manchester_Dev'
+
+      end
+
+      def assert_claim_export_started(export:)
+        jobs = Sidekiq::Worker.jobs.select do |j|
+          j['queue'] == 'events' && j['wrapped'] == 'TriggerEventJob' &&
+            j['args'].first['arguments'].first == 'ClaimExportFeedbackReceived' &&
+            JSON.parse(j['args'].first['arguments'][1]) >= {'state' => 'in_progress', 'export_id' => export.id, 'percent_complete' => 0}
+        end
+        expect(jobs.length).to be 1
+      end
+    end
+  end
+end
+
+RSpec.configure do |c|
+  c.include ::EtCcdExport::Test::ExternalEventsMethods
+end
