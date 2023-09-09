f0fab1b7372898164d41910a13742165a9ee8e66
garytaylor <gary.taylor@hismessages.com>
2023-09-09 14:24:41 +0100

    Rst 5651 - enabled coverage and upgraded rails (#329)
    * (RST-5651) Upgraded rails to allow bundle-audit to run
    
    * (RST-5651) Enabled coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,11 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'simplecov'
+require "simplecov_json_formatter"
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
+SimpleCov.start if ENV.fetch('ENABLE_COVERAGE', 'false').downcase == 'true'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
