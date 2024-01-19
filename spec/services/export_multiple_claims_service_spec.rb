7346f30b7ae5b50f8bc5eb8e8323195132109936
Aaron Lambley <aaron.lambley@gmail.com>
2024-01-19 09:54:17 +0000

    run rubocop -a

--- spec/services/export_multiple_claims_service_spec.rb
+++ spec/services/export_multiple_claims_service_spec.rb
@@ -71,7 +71,7 @@ RSpec.describe ExportMultipleClaimsService do
     end
 
     def primaryClaimantWorkAddressMatcher(_claimant, claim) # rubocop:disable Naming/MethodName
-      address = claim.primary_respondent.work_address.present? ? claim.primary_respondent.work_address : claim.primary_respondent.address
+      address = claim.primary_respondent.work_address.presence || claim.primary_respondent.address
       a_hash_including('claimant_work_address' => address_matcher(address, has_country: false))
     end
 
@@ -225,64 +225,64 @@ RSpec.describe ExportMultipleClaimsService do
     let(:test_ccd_client) { EtCcdClient::UiClient.new.tap { |c| c.login(username: 'm@m.com', password: 'Pa55word11') } }
     let(:example_ccd_data) do
       {
-        "receiptDate": "2019-06-12",
-        "feeGroupReference": "222000000100",
-        "claimant_TypeOfClaimant": "Individual",
-        "claimantIndType": {
-          "claimant_title1": "Mrs",
-          "claimant_first_names": "tamara",
-          "claimant_last_name": "swift",
-          "claimant_date_of_birth": "1957-07-06",
-          "claimant_gender": nil
+        receiptDate: "2019-06-12",
+        feeGroupReference: "222000000100",
+        claimant_TypeOfClaimant: "Individual",
+        claimantIndType: {
+          claimant_title1: "Mrs",
+          claimant_first_names: "tamara",
+          claimant_last_name: "swift",
+          claimant_date_of_birth: "1957-07-06",
+          claimant_gender: nil
         },
-        "claimantType": {
-          "claimant_addressUK": {
-            "AddressLine1": "71088",
-            "AddressLine2": "nova loaf",
-            "PostTown": "keelingborough",
-            "County": "hawaii",
-            "Country": nil,
-            "PostCode": "yy9a 2la"
+        claimantType: {
+          claimant_addressUK: {
+            AddressLine1: "71088",
+            AddressLine2: "nova loaf",
+            PostTown: "keelingborough",
+            County: "hawaii",
+            Country: nil,
+            PostCode: "yy9a 2la"
           },
-          "claimant_phone_number": nil,
-          "claimant_mobile_number": nil,
-          "claimant_email_address": nil,
-          "claimant_contact_preference": nil
+          claimant_phone_number: nil,
+          claimant_mobile_number: nil,
+          claimant_email_address: nil,
+          claimant_contact_preference: nil
         },
-        "caseType": "Multiple",
-        "multipleReference": "24000001/#{Time.now.year}",
-        "respondentSumType": {
-          "respondent_name": "dodgy_co",
-          "respondent_ACAS_question": "Yes",
-          "respondent_address": {
-            "AddressLine1": "1",
-            "AddressLine2": "street",
-            "PostTown": "locality",
-            "County": "county",
-            "PostCode": "post code"
+        caseType: "Multiple",
+        multipleReference: "24000001/#{Time.now.year}",
+        respondentSumType: {
+          respondent_name: "dodgy_co",
+          respondent_ACAS_question: "Yes",
+          respondent_address: {
+            AddressLine1: "1",
+            AddressLine2: "street",
+            PostTown: "locality",
+            County: "county",
+            PostCode: "post code"
           },
-          "respondent_phone1": "01234 567890",
-          "respondent_ACAS": "AC123456/78/90"
+          respondent_phone1: "01234 567890",
+          respondent_ACAS: "AC123456/78/90"
         },
-        "claimantWorkAddress": {},
-        "respondentCollection": [],
-        "claimantOtherType": {},
-        "claimantRepresentedQuestion": "Yes",
-        "representativeClaimantType": {
-          "representative_occupation": "Solicitor",
-          "name_of_organisation": "Org name",
-          "name_of_representative": "Rep Name",
-          "representative_address": {
-            "AddressLine1": "1",
-            "AddressLine2": "street",
-            "PostTown": "locality",
-            "County": "county",
-            "PostCode": "post code"
+        claimantWorkAddress: {},
+        respondentCollection: [],
+        claimantOtherType: {},
+        claimantRepresentedQuestion: "Yes",
+        representativeClaimantType: {
+          representative_occupation: "Solicitor",
+          name_of_organisation: "Org name",
+          name_of_representative: "Rep Name",
+          representative_address: {
+            AddressLine1: "1",
+            AddressLine2: "street",
+            PostTown: "locality",
+            County: "county",
+            PostCode: "post code"
           },
-          "representative_phone_number": "01234 565899",
-          "representative_mobile_number": "07771 666555",
-          "representative_email_address": "test@email.com",
-          "representative_dx": "dx1234567890"
+          representative_phone_number: "01234 565899",
+          representative_mobile_number: "07771 666555",
+          representative_email_address: "test@email.com",
+          representative_dx: "dx1234567890"
         }
       }
     end
