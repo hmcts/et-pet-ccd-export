bb11fbd52000de9158b4161613c1259bfa6c7686
Gary Taylor <gary.taylor@hismessages.com>
2026-08-02 15:09:58 +0100

    (rst-8374) Lib added to eager load path

--- lib/et_ccd_export/engine.rb
+++ lib/et_ccd_export/engine.rb
@@ -3,5 +3,8 @@ module EtCcdExport
     isolate_namespace EtCcdExport
     config.generators.api_only = true
     config.generators.test_framework = :rspec
+
+    config.autoload_paths << root.join('lib')
+    config.eager_load_paths << root.join('lib')
   end
 end
