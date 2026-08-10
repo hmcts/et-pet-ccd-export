34aef20be64c955f6b8b15d09f4ded4b2186a56a
Gary Taylor <gary.taylor@hismessages.com>
2026-08-10 10:48:09 +0100

    Disabled initializers when not in standalone mode

--- lib/et_ccd_export/engine.rb
+++ lib/et_ccd_export/engine.rb
@@ -6,5 +6,13 @@ module EtCcdExport
 
     config.autoload_paths << root.join('lib')
     config.eager_load_paths << root.join('lib')
+
+    # Disable initializers when used not as a standalone application for now
+    initializer "et_ccd_export.disable_embedded_initializers",
+                before: :load_config_initializers do |app|
+      unless app.class.name == "EtCcdExport::Application"
+        config.paths["config/initializers"] = []
+      end
+    end
   end
 end
